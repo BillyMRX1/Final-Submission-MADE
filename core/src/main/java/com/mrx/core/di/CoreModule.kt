137c9e3703de0522194091f0cb814122fd19903c
BillyMRX1 <brilianadeputra@gmail.com>
2021-05-30 15:42:54 +0800

    Update Gradle

--- core/src/main/java/com/mrx/core/di/CoreModule.kt
+++ core/src/main/java/com/mrx/core/di/CoreModule.kt
@@ -11,6 +11,7 @@ import com.mrx.core.domain.repository.IMangaRepository
 import com.mrx.core.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -34,11 +35,18 @@ val databaseModule = module {
 }
 
 val networkModule = module {
+    val hostname = "api.jikan.moe"
+    val certificatePinner = CertificatePinner.Builder()
+        .add(hostname, "sha256/7WzOtAYFrxzTuC2KKPH0qCk4EB5tukcc+aX96BTmFWI=")
+        .add(hostname, "sha256/jQJTbIh0grw0/1TkHSumWb+Fs0Ggogr621gT3PvPKG0=")
+        .add(hostname, "sha256/Vjs8r4z+80wjNcr1YKepWQboSIRi63WsWXhIMN+eWys=")
+        .build()
     single {
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
